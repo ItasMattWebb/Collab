31b0c6352c17266f025b3c6f8eb05d587868c296
logix1099 <ks2@live.ca>
2018-01-26 18:59:32 -0800

    help me

---
+++ Collab/Assets/pong/ball.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour {
+    public float startingVelocity = 10;
+	// Use this for initialization
+	void Start () {
+        
+        gameObject.GetComponent<Rigidbody2D>().AddForce(((Random.value >= 0.5) ? transform.right : -transform.right)* startingVelocity);
+
+
+            }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Collab/Assets/pong/player1paddle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player1paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, Input.GetAxis("Vertical"));
+       
+    }
+}
